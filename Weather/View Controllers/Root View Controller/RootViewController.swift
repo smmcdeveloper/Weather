ceae1200e86ec63e5523d44d4a71b46591fd2f7e
smmcdeveloper <laozi.develope@gmail.com>
2021-02-22 19:11:21 +0000

    Project setup

---
+++ Weather/View Controllers/Root View Controller/RootViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RootViewController.swift
+//  Weather
+//
+//  Created by SMMC on 22/02/2021.
+//
+
+import UIKit
+
+final class RootViewController: UIViewController {
+
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
